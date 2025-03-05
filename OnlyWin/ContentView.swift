29a79c8f256161dc1f3e8bf76b4bd273cae918ea
shturkhal <107053994+shturkhal@users.noreply.github.com>
2025-03-05 19:14:41 +0200

    Initial Commit

---
+++ OnlyWin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnlyWin
+//
+//  Created by Іван Штурхаль on 05.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnlyWin/OnlyWinApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlyWinApp.swift
+//  OnlyWin
+//
+//  Created by Іван Штурхаль on 05.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct OnlyWinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
